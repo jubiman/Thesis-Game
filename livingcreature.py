ba8d8432fc0c5df39ebb50bb134e1225de02f991
Melvinryan <melvinryan.boom@gmail.com>
2021-04-06 14:58:10 +0200

    Update with LivingCreature added

---
+++ livingcreature.py
@@ -0,0 +1,19 @@
+import pygame
+
+
+class LivingCreature(pygame.sprite.Sprite):
+	def __init__(self, game, hp, max_hp, armor):
+		self.groups = game.sprites
+
+		# Get the game's object so we can interact with the world
+		self.game = game
+
+# LivingCreature Base
+		self.hp = hp  # TODO: Ability to change the HP of player
+		self.max_hp = max_hp
+		self.armor = armor
+
+		# TODO: max_hp
+
+		# Initialize Sprite base
+		pygame.sprite.Sprite.__init__(self, self.groups)
