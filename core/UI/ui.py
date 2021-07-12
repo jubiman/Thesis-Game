864589ccc542c4df563eaa2621b225a011e425c0
Melle Bosboom <jubiman@users.noreply.github.com>
2021-07-12 16:41:17 +0200

    Update UI elements to work correctly
    Changelog:
     - yes
    Signed-off-by: Melle Bosboom <jubiman@users.noreply.github.com>

--- core/UI/ui.py
+++ core/UI/ui.py
@@ -12,3 +12,23 @@ class UI:
 	def draw(screen):
 		for obj in UI.uiElements:
 			obj.draw(screen)
+
+	@staticmethod
+	def getElementByID(iden):
+		"""
+		:param iden: The ID of the element (can also do UI.uiElements[id])
+		:return: UI element
+		:rtype: Any
+		"""
+		return UI.uiElements[iden]
+
+	@staticmethod
+	def getElementByStr(name):
+		"""
+		:param name: The name of the element
+		:return: None or the UI element
+		"""
+		for el in UI.uiElements:
+			if name.lower() == el.__class__.__name__.lower():
+				return el
+		return None
